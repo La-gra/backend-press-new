f6a1bcce0599c44045e40fd052267cb586a1b0eb
La-gra <ninajuccinte@gmail.com>
2026-05-15 17:33:59 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("PR NEWS API");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
